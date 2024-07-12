052ef0f3bd1a32237c5fcad7eff230b06cd3bc05
Gustavo Quinalha <gustavo_quinalha@hotmail.com>
2024-07-12 19:50:17 -0300

    add menu

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -6,13 +6,15 @@ import * as $$$$$$$$$$$0 from "./apps/deco/htmx.ts";
 import * as $$$$$$$$$$$1 from "./apps/site.ts";
 import * as $$$$$$0 from "./sections/Footer.tsx";
 import * as $$$$$$1 from "./sections/ItWorks.tsx";
-import * as $$$$$$2 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$2 from "./sections/Menu.tsx";
+import * as $$$$$$3 from "./sections/Theme/Theme.tsx";
 
 const manifest = {
   "sections": {
     "site/sections/Footer.tsx": $$$$$$0,
     "site/sections/ItWorks.tsx": $$$$$$1,
-    "site/sections/Theme/Theme.tsx": $$$$$$2,
+    "site/sections/Menu.tsx": $$$$$$2,
+    "site/sections/Theme/Theme.tsx": $$$$$$3,
   },
   "apps": {
     "site/apps/deco/htmx.ts": $$$$$$$$$$$0,
