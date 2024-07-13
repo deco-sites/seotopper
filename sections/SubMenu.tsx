a39bfdd360943820aafba423ebbb9fafd21dfdee
Gustavo Quinalha <gustavo_quinalha@hotmail.com>
2024-07-13 02:52:16 -0300

    ajustes

--- sections/SubMenu.tsx
+++ sections/SubMenu.tsx
@@ -1,15 +1,13 @@
 export default function Section() {
   return (
-    // data-theme="dark"
     <div
       class="bg-base-100 border-b border-accent h-14 w-full flex items-center justify-between px-4 gap-4"
       id="menu"
       data-theme="black"
     >
-      <div className="flex gap-2">
-        <button class="btn btn-sm btn-accent">Create</button>
-        <button class="btn btn-sm btn-primary">Preview</button>
-        <button class="btn btn-sm btn-primary">Export</button>
+      <div className="flex gap-0 h-full">
+        <button class="block h-full h-14 border-b hover:border-accent !border-white text-white btn-accent flex items-center justify-center text-center px-4 text-sm">Create</button>
+        <button class="block h-full h-14 border-b hover:border-accent border-transparent btn-primary flex items-center justify-center text-center px-4 text-sm">Preview</button>
       </div>
     </div>
   );
